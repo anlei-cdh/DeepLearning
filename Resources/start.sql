f85523c3304d7d9dca424c31debebc7b1b350d39
anlei-cdh <824652977@qq.com>
2019-11-04 22:01:04 +0800

    commit code

---
+++ Resources/start.sql
@@ -0,0 +1,7 @@
+#下载Anaconda3-4.2 (python3.5)
+https://repo.continuum.io/archive/ Anaconda3-4.2.0-Windows-x86_64.exe
+
+#安装opencv,pymysql
+https://pypi.org/project/opencv-python/
+pip install "D:\Program Files\Anaconda4.2\whl\opencv_python-3.4.4.19-cp35-cp35m-win_amd64.whl"
+pip install "D:\Program Files\Anaconda4.2\whl\PyMySQL-0.7.11-py2.py3-none-any.whl"
